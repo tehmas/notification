e4a96debfa6087c6e5a9c920d390e59eab923114
Asad Raheem <asad.raheem@almusnet.com>
2020-09-06 00:45:17 +0500

    refactored code for better readability

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,8 +3,8 @@ from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://mongo27017/notification'
+mongo = PyMongo(app)
 api = Api(app)
 
-from app.controller import user_controller
-
-api.add_resource(user_controller.UserController, '/')
+from app.controller import *
--- api/app/controller/user_controller.py
+++ api/app/controller/user_controller.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource
+from app import api
 
 class UserController(Resource):
     def get(self):
         return ["dummy"]
+
+def addResources(api):
+    api.add_resource(UserController, '/')
+
+addResources(api)
