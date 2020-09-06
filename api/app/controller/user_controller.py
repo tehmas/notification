26c1648078f80677b765c68ed88febbb3f4e55ad
Asad Raheem <asad.raheem@almusnet.com>
2020-09-06 18:39:04 +0500

    added apis for user creation and retrieval

--- api/app/controller/user_controller.py
+++ api/app/controller/user_controller.py
@@ -1,11 +1,39 @@
 from flask_restful import Resource
-from app import api
+from flask import request, jsonify
+from bson.objectid import ObjectId
+from app import api, mongo
+from pymongo import errors
 
 class UserController(Resource):
     def get(self):
-        return ["dummy"]
+        return str(mongo.db.user\
+            .find_one({'_id':ObjectId(request.args['id'])}))
+    
+    def post(self):
+        inputModel = request.get_json()
+        
+        (isValid, errorMsg) = validate_user_payload(inputModel)
+        if (isValid == False):
+            return errorMsg, 400
 
-def addResources(api):
-    api.add_resource(UserController, '/')
+        try:
+            result = mongo.db.user.insert_one(inputModel)
+            return {'Data':{'id':str(result.inserted_id)}}, 200
+        except errors.DuplicateKeyError:
+            return {'Error':'Record already exists'}, 500
 
-addResources(api)
+def validate_user_payload(payload):
+    if ('email' not in payload):
+        return False, {'Error': 'email is required'}
+    if ('firstName' not in payload):
+        return False, {'Error': 'firstName is required'}
+    return True, None
+
+def add_resources(api):
+    api.add_resource(UserController, '/user')
+
+def setup_indexes():
+    mongo.db.user.create_index("email", unique=True)
+
+setup_indexes()
+add_resources(api)
