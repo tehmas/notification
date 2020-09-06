c8b783f241858393359bfb4f58ce0d7d74c2613a
Asad Raheem <asad.raheem@almusnet.com>
2020-09-07 02:05:43 +0500

    added notification controller and rabbitmq service

--- api/app/controller/notification_controller.py
+++ api/app/controller/notification_controller.py
@@ -2,6 +2,10 @@ from flask_restful import Resource
 from flask import request, jsonify
 from app import api, mongo
 from marshmallow import Schema, fields, ValidationError
+import pika
+import os
+from functools import partial
+import json
 
 class NotificationSchema(Schema):
     title = fields.String(required=True)
@@ -21,9 +25,37 @@ class NotificationController(Resource):
         
         try:
             result = mongo.db.notification.insert_one(inputModel)
-            return {'Data':{'id':str(result.inserted_id)}}, 200
+            notification_id = str(result.inserted_id)
+            self.enqueue_notification(notification_id)
+            return {'Data':{'id':notification_id}}, 200
         except:
             return {'Error':'Could not send notification'}, 400
+
+    # TODO: A common utility class is needed
+    def enqueue_notification(self, notification_id):
+        parameters = pika.URLParameters(os.environ['RABBITMQ_AMQP_URL'])
+        connection = pika.SelectConnection(parameters, \
+            on_open_callback=partial(self.on_conn_open, notification_id))
+        connection.ioloop.start()
+    
+    def on_conn_open(self, notification_id, connection):
+        connection.channel(on_open_callback=\
+            partial(self.on_channel_open, notification_id, connection))
+
+    def on_channel_open(self, notification_id, connection, channel):
+        channel.queue_declare(queue='notification_queue',\
+            callback=partial(self.on_queue_declared,\
+                channel, connection, notification_id))
+
+    def on_queue_declared(self, channel, connection, notification_id, frame):
+        channel.basic_publish(exchange='',\
+            routing_key='notification_queue',\
+                body=notification_id,
+                properties=pika.BasicProperties(\
+                    delivery_mode=2, \
+                        content_type= 'text/plain'))
+        connection.close()
+        connection.ioloop.stop()
 
 def add_resources(api):
     api.add_resource(NotificationController, '/notification')
