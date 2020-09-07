4eabeea33b19f7ba2b557205e1ab6c762fa8fb1b
Asad Raheem <asad.raheem@almusnet.com>
2020-09-07 15:18:12 +0500

    changed database name

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://mongo:27017/notification'
+app.config['MONGO_URI'] = 'mongodb://mongo:27017/notificationdb'
 mongo = PyMongo(app)
 api = Api(app)
 
