f883e32e65342ebb6e53daaa728cdcdaa43aae24
Asad Raheem <asad.raheem@almusnet.com>
2020-09-07 15:54:50 +0500

    added MONGO_DB_NAME in environment

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
+import os
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://mongo:27017/notificationdb'
+app.config['MONGO_URI'] = 'mongodb://mongo:27017/'+\
+    os.environ['MONGO_DB_NAME']
 mongo = PyMongo(app)
 api = Api(app)
 
